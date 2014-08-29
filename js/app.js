3e6f8c5968504e7c029bbb01a185a8f6fbd59d67
Tony Ennis <tony.ennis@twc.com>
2014-08-28 23:49:33 -0400

    Added moar tests!

--- js/app.js
+++ js/app.js
@@ -10,7 +10,10 @@ angular.module('myApp', [
     'myApp.order_controller',
     'myApp.tab_member',
     'myApp.order_bits',
-    'myApp.services'
+    'myApp.services',
+    'myApp.order-sashing',
+    'myApp.order-size',
+    'myApp.order-backing-fabric'
 ]).
     config(['$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({redirectTo: '/view1'});
--- js/order_size_page.js
+++ js/order_size_page.js
@@ -90,34 +90,34 @@ angular.module('myApp.order_tabs', [])
 //            }
 //        };
 //    }]
-.controller('OrderSashingController', ['$scope', 'OrderSashingFactory', function ($scope, OrderSashingFactory) {
-        $scope.model = OrderSashingFactory;
-    }]).factory('OrderSashingFactory', ['OrderSizeFactory', 'OrderBackingFabricFactory', function (OrderSizeFactory, OrderBackingFabricFactory) {
-        return {
-            tabText: "3. Sashing Options",
-            partial: "partials/order3.html",
-            autoAccept: false,
-            sashing: undefined,
-            price: function () {
-                return (this.sashing && OrderSizeFactory.size >= 0) ? OrderSizeFactory.sizes[OrderSizeFactory.size].sashing : 0
-            },
-            color: function () {
-                return OrderBackingFabricFactory.backingFabricColor()
-            },
-            lineItem: function () {
-                var text = "No sashing",
-                    price = 0;
-                if (this.sashing) {
-                    text = "Add sashing";
-                    price = OrderSizeFactory.sizes[OrderSizeFactory.size].sashing;
-                }
-                return {text: text, price: price};
-            },
-            narrative: function () {
-                return this.sashing ? " and sash" : ""
-            }
-        };
-    }]).controller("OrderThreadController", ['$scope', 'OrderThreadFactory', function ($scope, OrderThreadFactory) {
+//.controller('OrderSashingController', ['$scope', 'OrderSashingFactory', function ($scope, OrderSashingFactory) {
+//        $scope.model = OrderSashingFactory;
+//    }]).factory('OrderSashingFactory', ['OrderSizeFactory', 'OrderBackingFabricFactory', function (OrderSizeFactory, OrderBackingFabricFactory) {
+//        return {
+//            tabText: "3. Sashing Options",
+//            partial: "partials/order3.html",
+//            autoAccept: false,
+//            sashing: undefined,
+//            price: function () {
+//                return (this.sashing && OrderSizeFactory.size >= 0) ? OrderSizeFactory.sizes[OrderSizeFactory.size].sashing : 0
+//            },
+//            color: function () {
+//                return OrderBackingFabricFactory.backingFabricColor()
+//            },
+//            lineItem: function () {
+//                var text = "No sashing",
+//                    price = 0;
+//                if (this.sashing) {
+//                    text = "Add sashing";
+//                    price = OrderSizeFactory.sizes[OrderSizeFactory.size].sashing;
+//                }
+//                return {text: text, price: price};
+//            },
+//            narrative: function () {
+//                return this.sashing ? " and sash" : ""
+//            }
+//        };
+    .controller("OrderThreadController", ['$scope', 'OrderThreadFactory', function ($scope, OrderThreadFactory) {
         $scope.model = OrderThreadFactory;
     }]).factory('OrderThreadFactory', function () {
         return {
